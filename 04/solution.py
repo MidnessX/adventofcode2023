7430827df95280806ab1fa431439f839f97e8e9e
Marco Bettini <72510075+MidnessX@users.noreply.github.com>
2023-12-04 12:07:37 +0100

    Solution 04 pt. 2.

--- 04/solution.py
+++ 04/solution.py
@@ -7,9 +7,21 @@ num_re = re.compile("(\d+)")
 with open("04/input.txt") as table_f:
     sum_card_values = 0
 
+    card_copies = list()
+
     for scratch_card in table_f.readlines():
         splits = scratch_card.split(":")
-        card_id = splits[0]
+
+        card_id = int(num_re.search(splits[0]).group())
+
+        if card_id == 71:
+            pass
+
+        if len(card_copies) >= card_id:
+            card_copies[card_id - 1] += 1
+        else:
+            card_copies.append(1)  # We have at least one copy of each scratchcard
+
         sequences = splits[1].split("|")
 
         winning_seq = sequences[0]
@@ -20,8 +32,16 @@ with open("04/input.txt") as table_f:
 
         matches = len(winning_nos.intersection(card_nos))
 
+        if matches > 0:
+            for i in range(card_id, card_id + matches):
+                if len(card_copies) > i:
+                    card_copies[i] += card_copies[card_id - 1]
+                else:
+                    card_copies.append(card_copies[card_id - 1])
+
         card_value = 2 ** (matches - 1) if matches > 0 else 0
 
         sum_card_values += card_value
 
     print(f"Sum of card values: {sum_card_values}")
+    print(f"Total number of scratchcards: {sum(card_copies)}")
