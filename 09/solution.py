d22c0b34a04ba6986768407d77e86c1466e89939
Marco Bettini <72510075+MidnessX@users.noreply.github.com>
2023-12-09 18:57:19 +0100

    Solution 09 pt. 1.

---
+++ 09/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+
+def predict(sequence: list[int]) -> int:
+    i = 0
+    history = [sequence[-1]]
+
+    while any(sequence):
+        sequence = [sequence[j + 1] - sequence[j] for j in range(len(sequence) - 1)]
+        history.append(sequence[-1])
+
+        i += 1
+
+    prediction = 0
+    for val in history:
+        prediction += val
+
+    return prediction
+
+
+with open(Path(__file__).parent / "input.txt") as seq_f:
+    sequences = [[int(x) for x in line.split(" ")] for line in seq_f.readlines()]
+
+predictions = map(predict, sequences)
+
+print(f"Sum of predictions: {sum(predictions)}")
