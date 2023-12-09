183e6e8829284aeb016fe3748c6276cbf4043618
Marco Bettini <72510075+MidnessX@users.noreply.github.com>
2023-12-09 19:31:53 +0100

    Solution 09 pt. 2.

--- 09/solution.py
+++ 09/solution.py
@@ -3,15 +3,20 @@
 from pathlib import Path
 
 
-def predict(sequence: list[int]) -> int:
-    i = 0
-    history = [sequence[-1]]
+def predict(sequence: list[int], start: bool = False) -> int:
+    history = [sequence[0]] if start else [sequence[-1]]
 
     while any(sequence):
         sequence = [sequence[j + 1] - sequence[j] for j in range(len(sequence) - 1)]
-        history.append(sequence[-1])
 
-        i += 1
+        history.append(sequence[0] if start else sequence[-1])
+
+    if start:
+        pred = 0
+        for i in range(len(history) - 1, -1, -1):
+            pred = history[i] - pred
+
+        return pred
 
     return sum(history)
 
@@ -19,6 +24,10 @@ def predict(sequence: list[int]) -> int:
 with open(Path(__file__).parent / "input.txt") as seq_f:
     sequences = [[int(x) for x in line.split(" ")] for line in seq_f.readlines()]
 
-predictions = map(predict, sequences)
+# Part 1
+end_predictions = map(predict, sequences)
+# Part 2
+start_predictions = map(lambda sequence: predict(sequence, start=True), sequences)
 
-print(f"Sum of predictions: {sum(predictions)}")
+print(f"Sum of end predictions: {sum(end_predictions)}")
+print(f"Sum of start predictions: {sum(start_predictions)}")
