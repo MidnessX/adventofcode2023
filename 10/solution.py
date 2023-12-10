08f60714a06d8197bafed33ce5b9c76178a581bf
Marco Bettini <72510075+MidnessX@users.noreply.github.com>
2023-12-10 15:39:53 +0100

    Solution 10 pt 2.

--- 10/solution.py
+++ 10/solution.py
@@ -121,6 +121,54 @@ def navigate(pipe: Pipe, start: Pipe, pipes: list[list[Pipe]]) -> list[Pipe]:
     return path
 
 
+def substitute_start(start: Pipe, prev: Pipe, next: Pipe) -> None:
+    candidates = set(["|", "L", "J", "7", "F", "-"])
+
+    for comp in [prev, next]:
+        if comp.x == start.x:
+            candidates -= set(["|"])
+        elif comp.x == start.x - 1:
+            candidates -= set(["7", "F", "-"])
+        else:
+            candidates -= set(["L", "J", "-"])
+
+        if comp.y == start.y:
+            candidates -= set(["-"])
+        elif comp.y == start.y - 1:
+            candidates -= set(["|", "L", "F"])
+        else:
+            candidates -= set(["|", "J", "7"])
+
+    if len(candidates) > 1:
+        raise ValueError("More than one possibilities left.")
+
+    return candidates.pop()
+
+
+def count_inside(
+    pipes: list[list[Pipe]], loop: list[Pipe], boundaries: tuple[int, int, int, int]
+) -> int:
+    x_min, y_min, x_max, y_max = boundaries
+
+    tiles_inside = 0
+
+    for i in range(x_min, x_max):
+        inside = False
+
+        for j in range(y_min, y_max):
+            tile = pipes[i][j]
+
+            if tile in loop:
+                if tile.type in ["|", "L", "J"]:
+                    inside = not inside
+                continue
+
+            if inside:
+                tiles_inside += 1
+
+    return tiles_inside
+
+
 pipes = list()
 start = None
 
@@ -141,7 +189,46 @@ with open(Path(__file__).parent / "input.txt") as map_f:
 
         x += 1
 
+# Part 1
+
 # path = navigate_recursive(start, None, start, pipes)
 path = navigate(start, start, pipes)
 
 print(f"Steps to reach farthest point in loop: {len(path) // 2}")
+
+# Part 2
+# If we assume the loop to have a direction (e.g. clockwise), a tile will be
+# inside the loop if it's found to the right of the loop tile.
+# This allows us to count the number of loop tiles encountered to the left of
+# a tile to determine whether or not it is inside the loop (i.e. even = outside,
+# odd = inside).
+# More precisely, since we are dealing with a grid, we can restrict our
+# calculations to just one dimension. We chose the X axis (i.e. rows) for
+# simplicity.
+# This allows us to only consider loop pipes of type |, L, or J, as those are
+# ones indicating a change of direction in the loop along the row.
+#
+# e.g.
+# |..|.||
+# means that the first pipe on the left was travelled bottom-up, the second pipe
+# top-down. the third bottom-up and the fourht bottom-up again.
+# The first two . are thus inside the loop, while the third . is outside.
+#
+# e.g.
+# F-7.F-7
+# |.|.|.|
+# # means that, on the first row, the only . is outside, while on the second row
+# # the first and the third . are inside and the second one is outside.
+#
+# The only caveat is with the starting node, S, which must be transformed into
+# the real type for this approach to work correctly.
+substitute_start(start, path[-1], path[1])
+
+x_min = min(path, key=lambda pipe: pipe.x)
+x_max = max(path, key=lambda pipe: pipe.x)
+y_min = min(path, key=lambda pipe: pipe.y)
+y_max = max(path, key=lambda pipe: pipe.y)
+
+tiles_inside = count_inside(pipes, path, (x_min.x, y_min.y, x_max.x + 1, y_max.y + 1))
+
+print(f"Tiles inside the loop: {tiles_inside}")
