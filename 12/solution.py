2e7633d37bd3fd28650c15d04bb96e0180ba8a4e
MidnessX <72510075+MidnessX@users.noreply.github.com>
2023-12-13 16:32:00 +0100

    Improved solution 12 pt. 1 by caching results.

--- 12/solution.py
+++ 12/solution.py
@@ -1,62 +1,49 @@
 #!/usr/bin/env python3
 
 import re
+from functools import lru_cache
 from pathlib import Path
 
 fault_re = re.compile(r"(#+)")
 
 
-def is_possible(line: str, faults: list[int], unkw_n: int) -> bool:
-    # The logic of this function can be further refined to identify impossible
-    # substitutions earlier on, but it is an error-prone task.
-    # We chose to trade speed for simplicity and mark a sequence as incompatible
-    # only when no more substitutions are possible and there's a mismatch
-    # between the number of faults and the faults found in the sequence.
+@lru_cache(maxsize=None)
+def get_faults(line: str) -> tuple[int, ...]:
+    f_found = tuple([len(m.group()) for m in fault_re.finditer(line)])
 
-    if unkw_n != 0:
-        return True
-
-    f_found = [len(m.group()) for m in fault_re.finditer(line)]
-
-    if len(f_found) == len(faults) and f_found == faults:
-        return True
-
-    return False
+    return f_found
 
 
-def permutations(line: str, pos: int, counts: list[int]) -> list[str]:
-    if pos == len(line):
-        if is_possible(line, counts, 0):
-            return [line]
-        return list()
+@lru_cache(maxsize=None)
+def permutations(line: str) -> tuple[str, ...]:
+    if len(line) == 1:
+        if line[0] == "?":
+            return (".", "#")
+        return line
 
-    if line[pos] != "?":
-        return permutations(line, pos + 1, counts)
+    perms = list()
 
-    res = list()
+    if line[0] == "?":
+        for subst in [".", "#"]:
+            perms.extend([subst + p for p in permutations(line[1:])])
+    else:
+        perms.extend([line[0] + p for p in permutations(line[1:])])
 
-    for subst in [".", "#"]:
-        line = line[: max(0, pos)] + subst + line[min(pos + 1, len(line)) :]
-        unkw_n = (
-            sum(map(lambda c: c == "?", line[pos + 1 :])) if pos < len(line) - 1 else 0
-        )
-
-        if not is_possible(line, counts, unkw_n):
-            continue
-
-        x = permutations(line, pos + 1, counts)
-        res.extend(x)
-
-    return res
+    return tuple(perms)
 
 
-poss = list()
+poss = 0
 
 with open(Path(__file__).parent / "input.txt") as diag_f:
     for line in diag_f.readlines():
         chars, counts = line.split(" ")
-        counts = [int(c) for c in counts.split(",")]
+        counts = tuple([int(c) for c in counts.split(",")])
 
-        poss.extend(permutations(chars, 0, counts))
+        for p in permutations(chars):
+            if len(p) != len(chars):
+                raise ValueError()
 
-print(f"Total possibilities: {len(poss)}.")
+            if get_faults(p) == counts:
+                poss += 1
+
+print(f"Total possibilities: {poss}.")
