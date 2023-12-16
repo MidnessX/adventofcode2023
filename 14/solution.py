4b65bb030609e6232649ac62c14a8d42cfada6c0
Marco Bettini <72510075+MidnessX@users.noreply.github.com>
2023-12-16 22:10:18 +0100

    Solution 1 improvements.

--- 14/solution.py
+++ 14/solution.py
@@ -3,23 +3,36 @@
 from pathlib import Path
 
 
-with open(Path(__file__).parent / "input.txt") as pltf_f:
-    length = len(pltf_f.readline().rstrip())
-    pltf_f.seek(0)
-    rows = len(pltf_f.readlines())
-    pltf_f.seek(0)
+def tilt_north(platform: list[list[str]]) -> None:
+    empty_pos = [0] * len(platform)
 
-    empty_pos = [-1] * length
-    total_load = 0
-
-    for i, line in enumerate(pltf_f.readlines()):
-        line = line.rstrip()
-
-        for j, item in enumerate(line):
+    for i, row in enumerate(platform):
+        for j, item in enumerate(row):
             if item == "#":
-                empty_pos[j] = i
+                empty_pos[j] = i + 1
             if item == "O":
+                platform[empty_pos[j]][j] = "O"
+                if empty_pos[j] != i:
+                    platform[i][j] = "."
+
                 empty_pos[j] += 1
-                total_load += rows - empty_pos[j]
+
+
+def weight(platform: list[str]) -> int:
+    total_weight = 0
+
+    for i, row in enumerate(platform):
+        for item in row:
+            if item == "O":
+                total_weight += len(platform) - i
+
+    return total_weight
+
+
+with open(Path(__file__).parent / "input.txt") as pltf_f:
+    pltf = [list(line.rstrip()) for line in pltf_f.readlines()]
+
+tilt_north(pltf)
+total_load = weight(pltf)
 
 print(f"Total platform load: {total_load}")
