2e8571457afef1c965a2cbf0ee5c350280de16e0
MidnessX <72510075+MidnessX@users.noreply.github.com>
2023-12-19 13:59:23 +0100

    Solution 17 pt. 2.

--- 17/solution.py
+++ 17/solution.py
@@ -4,11 +4,31 @@ import heapq
 from pathlib import Path
 
 
-def get_neighbours(x, y, dx, dy, s, max_x, max_y) -> list[tuple[int, int]]:
-    n = [(x - dy, y + dx), (x + dy, y - dx)]
+def get_neighbours(
+    x: int,
+    y: int,
+    dx: int,
+    dy: int,
+    s: int,
+    min_s: int,
+    max_s: int,
+    max_x: int,
+    max_y: int,
+) -> list[tuple[int, int]]:
+    n = list()
 
-    if s < 3:
-        n.append([x + dx, y + dy])
+    if dx == 0 and dy == 0:
+        # We are at the start. We can move in any direction.
+        n.append((x, y + 1))
+        n.append((x, y - 1))
+        n.append((x + 1, y))
+        n.append((x - 1, y))
+    else:
+        if s >= min_s:
+            n.append((x - dy, y + dx))
+            n.append((x + dy, y - dx))
+        if s < max_s:
+            n.append((x + dx, y + dy))
 
     n = filter(lambda t: t[0] >= 0 and t[0] < max_x and t[1] >= 0 and t[1] < max_y, n)
 
@@ -16,9 +36,25 @@ def get_neighbours(x, y, dx, dy, s, max_x, max_y) -> list[tuple[int, int]]:
 
 
 def find_path(
-    start: tuple[int, int], end: tuple[int, int], grid: list[list[int]]
+    start: tuple[int, int],
+    end: tuple[int, int],
+    grid: list[list[int]],
+    max_s: int,
+    min_s: int = 0,
 ) -> int:
-    nodes = [(0, *start, 0, 1, 0)]
+    # This is a weird Djikstra since we have constraints on the neighbours we
+    # can consider depending on previous moves.
+    # This means a node can be considered multiple times as long as the
+    # direction we came from and the number of steps in that direction differ
+    # from those seen previously.
+
+    # In nodes, our priority queue, we keep the cost, the coordinates, the
+    # direction we came from and the number of steps we already took in that
+    # direction.
+    nodes = [(0, *start, 0, 0, 0)]
+    # Visited holds the nodes we already explored and the configuration by
+    # which they got explored (i.e. the direction and the number of steps in
+    # that direction already taken).
     visited = set()
 
     while nodes:
@@ -29,10 +65,12 @@ def find_path(
 
         visited.add((x, y, dx, dy, s))
 
-        if (x, y) == end:
+        if (x, y) == end and s >= min_s:
             return c
 
-        for nx, ny in get_neighbours(x, y, dx, dy, s, len(grid), len(grid[0])):
+        for nx, ny in get_neighbours(
+            x, y, dx, dy, s, min_s, max_s, len(grid), len(grid[0])
+        ):
             nc = c + grid[nx][ny]
             ndx = nx - x
             ndy = ny - y
@@ -48,6 +86,8 @@ with open(Path(__file__).parent / "input.txt") as city_f:
         row = [int(line[y]) for y in range(len(line))]
         city.append(row)
 
-loss = find_path((0, 0), (len(city) - 1, len(city[0]) - 1), city)
+loss = find_path((0, 0), (len(city) - 1, len(city[0]) - 1), city, max_s=3)
+print(f"Total loss (normal crucible): {loss}.")
 
-print(f"Total loss: {loss}.")
+loss = find_path((0, 0), (len(city) - 1, len(city[0]) - 1), city, max_s=10, min_s=4)
+print(f"Total loss (ultra crucible): {loss}.")
