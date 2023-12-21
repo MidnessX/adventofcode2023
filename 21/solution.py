755fa7afc476cd271735591df5496462b6895187
MidnessX <72510075+MidnessX@users.noreply.github.com>
2023-12-21 12:29:57 +0100

    Solution 21 pt. 2.

--- 21/solution.py
+++ 21/solution.py
@@ -5,40 +5,47 @@ from pathlib import Path
 import numpy as np
 
 
-def neighbouring_tiles(
-    tile: tuple[int, int], garden: np.ndarray
+def neighbouring_plots(
+    tile: tuple[int, int], garden: np.ndarray, cycle: bool = False
 ) -> list[tuple[int, int]]:
-    candidates = [
-        (tile[0] + d[0], tile[1] + d[1]) for d in [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    ]
+    candidates = list()
 
-    candidates = list(
-        filter(
-            lambda t: 0 <= t[0] < garden.shape[0]
-            and 0 <= t[1] < garden.shape[1]
-            and garden[t] != "#",
-            candidates,
-        )
-    )
+    for d in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        c = (tile[0] + d[0], tile[1] + d[1])
+
+        if not cycle and not (
+            0 <= c[0] < garden.shape[0] and 0 <= c[1] < garden.shape[1]
+        ):
+            continue
+
+        # We use the modulo value to look up the equivalent plot in an adjacent
+        # garden tile.
+        if garden[(c[0] % garden.shape[0], c[1] % garden.shape[1])] == "#":
+            continue
+
+        candidates.append(c)
 
     return candidates
 
 
-def reachable_tiles(
-    start: tuple[int, int], steps: int, garden: np.ndarray
+def reachable_plots(
+    start: list[tuple[int, int]], garden: np.ndarray, infinite_garden: bool = False
 ) -> set[tuple[int, int]]:
-    edge_tiles = set([start])
+    reached_plots = set()
 
-    for _ in range(steps):
-        reached_tiles = set()
+    for t in start:
+        neigh = neighbouring_plots(t, garden, cycle=infinite_garden)
+        reached_plots.update(neigh)
 
-        for t in edge_tiles:
-            neigh = neighbouring_tiles(t, garden)
-            reached_tiles.update(neigh)
+    return reached_plots
 
-        edge_tiles = reached_tiles
 
-    return edge_tiles
+def interpolate(y: list[tuple[int, int]], x: int) -> float:
+    a = (y[2] + y[0] - 2 * y[1]) / 2
+    b = y[1] - y[0] - a
+    c = y[0]
+
+    return a * x**2 + b * x + c
 
 
 with open(Path(__file__).parent / "input.txt") as map_f:
@@ -60,6 +67,37 @@ with open(Path(__file__).parent / "input.txt") as map_f:
 
 garden = np.asarray(garden)
 
-tiles = reachable_tiles(start, 64, garden)
+# Part 1
+goal = 64
+starting_plots = set([start])
+for _ in range(goal):
+    plots = reachable_plots(starting_plots, garden, infinite_garden=False)
+    starting_plots = plots
 
-print(f"Reachable tiles: {len(tiles)}.")
+print(f"Reachable plots after {goal} steps: {len(starting_plots)}.")
+
+# Part 2
+# The trick here is realizing that the number of reachable plots increases
+# quadratically with the number of garden widths walked.
+# This is due to how the garden is laid out: edges don't contain rocks, the
+# starting point is in the middle and there are no rocks in the row and column
+# where the starting point is located. Moreover, diagonal lines connecting
+# the middle points of the edges of the garden are also free of rocks.
+# This translates to reachable plots expanding in a diamond pattern from the
+# center.
+# So what we can do is find the first three values of reachable plots and
+# use them to fit a quadratic polynomial. Then, we can simply use this
+# polynomial to interpolate the number of reachable plots after the desired
+# number of steps.
+
+goal = 26501365
+starting_plots = set([start])
+y = list()
+for i in range(1, garden.shape[0] * 3):
+    plots = reachable_plots(starting_plots, garden, infinite_garden=True)
+    starting_plots = plots
+
+    if i % garden.shape[0] == goal % garden.shape[0]:
+        y.append(len(plots))
+
+print(f"Reachable tiles after {goal} steps: {interpolate(y, goal // garden.shape[0])}.")
